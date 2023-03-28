fc1e6637093a69ac392eee3d0bb8eceee06de7d6
Alvin Galit <aroblesgalit@gmail.com>
2023-03-27 20:09:49 -0700

    Created component that uses params in route

---
+++ nuxt-3/pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium assumenda sed expedita quod alias pariatur eius harum magni officia numquam!</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
