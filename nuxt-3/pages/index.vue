3ed5d39252b7a758037913abebe3c18141f06999
Alvin Galit <aroblesgalit@gmail.com>
2023-03-28 20:20:48 -0700

    Created layout to include the header

--- nuxt-3/pages/index.vue
+++ nuxt-3/pages/index.vue
@@ -1,16 +1,5 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <NuxtLink to="/">Nuxt Dogo</NuxtLink>
-                <ul>
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li><NuxtLink to="/products">Products</NuxtLink></li>
-                </ul>
-            </nav>
-        </header>
-
         <h2>Home</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A delectus dolores eum! Aspernatur odio culpa consequatur rem voluptatibus dignissimos pariatur?</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A delectus dolores eum! Aspernatur odio culpa consequatur rem voluptatibus dignissimos pariatur?</p>
