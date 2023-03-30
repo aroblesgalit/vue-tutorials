84a8e452380b2335387b0ad63fe42256efc394cf
Alvin Galit <aroblesgalit@gmail.com>
2023-03-29 17:05:25 -0700

    Created a dynamic server route

--- nuxt-3/pages/about.vue
+++ nuxt-3/pages/about.vue
@@ -3,11 +3,12 @@
         <h2>About</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A delectus dolores eum! Aspernatur odio culpa consequatur rem voluptatibus dignissimos pariatur?</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A delectus dolores eum! Aspernatur odio culpa consequatur rem voluptatibus dignissimos pariatur?</p>
+        <div>{{ data }}</div>
     </div>
 </template>
 
 <script setup>
-
+    const { data } = await useFetch('/api/ninja?name=mario')
 </script>
 
 <style scoped>
