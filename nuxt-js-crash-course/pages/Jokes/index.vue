07c7cfb35aa4600cc4058e1a39445360f219c592
Alvin Galit <aroblesgalit@gmail.com>
2023-05-24 23:31:16 -0700

    Created data for jokes and fetched from api in a created lifecycle method

--- nuxt-js-crash-course/pages/Jokes/index.vue
+++ nuxt-js-crash-course/pages/Jokes/index.vue
@@ -5,9 +5,42 @@
 </template>
 
 <script>
-    export default {
-        
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            jokes: []
+        }
+    },
+    async created() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.get('https://icanhazdadjoke.com/search', config);
+            this.jokes = res.data.results;
+            
+        } catch (err) {
+            console.log(err)
+        }
+    },
+    head() {
+        return {
+            title: 'Dad Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for corny dad jokes'
+                }
+            ]
+        }
     }
+}
 </script>
 
 <style lang="scss" scoped>
