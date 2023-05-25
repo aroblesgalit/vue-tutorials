cf8c9d92791219520054523dbadfd880004e0373
Alvin Galit <aroblesgalit@gmail.com>
2023-05-24 23:38:44 -0700

    Imported Joke component and added props

--- nuxt-js-crash-course/pages/Jokes/index.vue
+++ nuxt-js-crash-course/pages/Jokes/index.vue
@@ -1,13 +1,22 @@
 <template>
     <div>
-        jokes
+        <Joke 
+            v-for="joke in jokes" 
+            :key="joke.id" 
+            :id="joke.id" 
+            :joke="joke.joke" 
+        />
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Joke from '../../components/Joke.vue'
 
 export default {
+    components: {
+        Joke
+    },
     data() {
         return {
             jokes: []
@@ -23,7 +32,7 @@ export default {
         try {
             const res = await axios.get('https://icanhazdadjoke.com/search', config);
             
-            this.jokes = res.data
+            this.jokes = res.data.results;
         } catch (err) {
             console.log(err)
         }
