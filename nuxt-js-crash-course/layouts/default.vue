3b7948be2815596a7efcaf7d6dcee48834cc6473
Alvin Galit <aroblesgalit@gmail.com>
2023-05-24 23:23:01 -0700

    Added styles

--- nuxt-js-crash-course/layouts/default.vue
+++ nuxt-js-crash-course/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <AppHeader />
         <nuxt />
     </div>
@@ -16,6 +16,34 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
 
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    line-height: 1.6;
+    background: #f4f4f4;
+}
+
+a {
+    color: #666;
+    text-decoration: none;
+}
+
+ul {
+    list-style: none;;
+}
+
+.container {
+    max-width: 800px;
+    margin: 2rem auto;
+    overflow: hidden;
+    padding: 1rem 2rem;
+    background: #fff;
+}
 </style>
