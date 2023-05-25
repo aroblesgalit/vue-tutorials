4ae367bbbbbc13165a731745aa6aa3b4c4d95768
Alvin Galit <aroblesgalit@gmail.com>
2023-05-24 22:54:34 -0700

    Created an about page

---
+++ nuxt-js-crash-course/pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>About DadJokes</h1>
+        <p>This is an app that displays corny dad jokes</p>
+    </div>
+</template>
+
+<script>
+
+    
+</script>
+
+<style scoped>
+
+</style>
