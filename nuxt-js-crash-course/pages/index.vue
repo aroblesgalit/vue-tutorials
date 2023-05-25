27cc336ae74a32a63d66c9a260c5956e8f8b1f52
Alvin Galit <aroblesgalit@gmail.com>
2023-05-24 23:09:00 -0700

    Created header component

--- nuxt-js-crash-course/pages/index.vue
+++ nuxt-js-crash-course/pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h2>Welcome to the best corny dad jokes app on the web</h2>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+import {defineComponent} from 'vue'
+export default defineComponent({
+    head() {
+        return {
+            title: 'Welcome to Dad Jokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place for corny dad jokes'
+                }
+            ]
+        }
+    }
+})
 </script>
